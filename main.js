e96d6959645eb9f2613a4eb064bf4bcec3191707
ellenguo6 <ellenguo6@gmail.com>
2023-10-02 23:22:02 -0400

    more chickensss

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 });
 
-const n = 3;
+const n = 7;
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
